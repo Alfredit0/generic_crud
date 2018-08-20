3d570521c460e6a82a21b5f282d277946e3d9975
Alfredo <benitoalfredoreyes@gmail.com>
2018-08-20 14:19:24 -0500

    index contain

--- index.php
+++ index.php
@@ -16,7 +16,37 @@ include 'common/head.php';
             include 'common/header.php';
         ?>
         <div class="container-fluid" style="font-size: 12px; margin-top: 1em;">
-
+        <div class="header-w3l">
+            <h1>
+                Sistema de Envío de Correos	Electrónicos (SECE)		
+            </h1>
+        </div>
+        <!--//header-->
+        <div class="main-content-agile">
+            <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+            <!-- Style-CSS -->
+            <link rel="stylesheet" href="css/font-awesome.css">
+            <!-- Font-Awesome-Icons-CSS -->
+            <div class="sub-main-w3">
+                <h2>Actividades</h2>
+                <div class="right-w3l"> 
+                    <input type="button" value="Añadir contactos" onclick="location.href = 'add.html';">                            
+                </div>                        
+                <div class="right-w3l">
+                    <p id="statusEnvio" class="center-block"></p>
+                    <input type="button" value="Enviar correos">
+                </div>	
+                <div class="right-w3l">   
+                    <input type="button" value="Cargar contactos desde excel" onclick="location.href = 'subirexcel.jsp';">
+                </div>
+                <!--//main-->
+                <!--footer-->
+                <div class="footer">
+                    <p>&copy; 2018 Alfredo Reyes
+                    </p>
+                </div>
+            </div>
+        </div>
         </div>
     <?php
         include 'common/footer.php';
